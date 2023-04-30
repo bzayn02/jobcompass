1ea10fd3728f306a11e5aca713a96954a0656047
bzayn02 <nagarkotibezay@gmail.com>
2023-04-30 12:27:17 +1000

    updated db

--- api-job-compass/src/config/db.js
+++ api-job-compass/src/config/db.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export const dbConnection = () => {
     try {
-        const connString = 'mongodb://localhost:27017/expenses_tracker';
+        const connString =
+            'mongodb+srv://vulpeshprajan:dB10Af0F8ss62MMI@cluster0.nozxmew.mongodb.net/?retryWrites=true&w=majority';
         const con = mongoose.connect(connString);
         con && console.log('Connected to MongoDB.');
     } catch (error) {
